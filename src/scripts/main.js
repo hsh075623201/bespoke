96c2904974257b1a86f80d4a1ba541f883c581a0
hsh075623201 <hsh.0223.ok@163.com>
2015-05-21 12:16:44 +0800

    revise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,4 +27,5 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
+console.log("...")
 
